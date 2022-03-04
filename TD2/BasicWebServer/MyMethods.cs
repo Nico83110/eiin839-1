e913f18ca94c09f606df114cd5d3a94d98947c5a
Nico83110 <nicolas.perrin83110@gmail.com>
2022-03-04 11:03:29 +0100

    first steps implemented

---
+++ TD2/BasicWebServer/MyMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer
+{
+    internal class MyMethods
+    {
+        public string method1(string var1, string var2)
+        {
+            return $"<html><body>Voici variable1: {var1} et variable2: {var2}</body></html>";
+        }
+    }
+}
--- TD2/BasicWebServer/Program.cs
+++ TD2/BasicWebServer/Program.cs
@@ -4,12 +4,35 @@ using System.IO;
 using System.Net;
 using System.Text;
 using System.Web;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace BasicServerHTTPlistener
+
 {
+    public class MyMethods
+    {
+        public string method1(string var1, string var2)
+        {
+            return $"<html><body>Hello {var1} et {var2}</body></html>";
+        }
+
+        public string method2(string res)
+        {
+            return $"<html><body>Hello {res}</body></html>";
+        }
+
+
+        public string callExternalApp(string var1, string var2)
+        {
+            Process.Start("C:\\");
+            return "";
+        }
+    }
+
     internal class Program
     {
-        private static void Main(string[] args)
+        private static async void Main(string[] args)
         {
 
             //if HttpListener is not supported by the Framework
@@ -64,6 +87,14 @@ namespace BasicServerHTTPlistener
                 HttpListenerRequest request = context.Request;
 
                 string documentContents;
+               /*Type type = typeof(MyMethods);
+                MethodInfo method = type.GetMethod("method1");
+                MyMethods m = new MyMethods();
+
+                HttpListenerRequest[] params = new HttpListenerRequest[1];
+                params[0] = request;
+                string result = (string)method.Invoke(m, params);*/
+
                 using (Stream receiveStream = request.InputStream)
                 {
                     using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
@@ -71,7 +102,20 @@ namespace BasicServerHTTPlistener
                         documentContents = readStream.ReadToEnd();
                     }
                 }
+
+                //Console.WriteLine(m.method2(documentContents));
+
+                // get url 
+                //Console.WriteLine($"Received request for {request.Url}");
+                //var response = context.Response;
+                //await using var stream = response.OutputStream;
+                //await using var sw = new StreamWriter(stream);
+
+                    //await HandleRequest(request, response);
+                    //response.StatusCode = (int)HttpStatusCode.OK;
                 
+        
+
                 // get url 
                 Console.WriteLine($"Received request for {request.Url}");
 
@@ -108,18 +152,42 @@ namespace BasicServerHTTPlistener
                 // Obtain a response object.
                 HttpListenerResponse response = context.Response;
 
-                // Construct a response.
-                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-                // Get a response stream and write the response to it.
-                response.ContentLength64 = buffer.Length;
-                System.IO.Stream output = response.OutputStream;
-                output.Write(buffer, 0, buffer.Length);
-                // You must close the output stream.
-                output.Close();
+                requestHandler(context, request);
             }
             // Httplistener neither stop ... But Ctrl-C do that ...
             // listener.Stop();
         }
+
+        public static void requestHandler(HttpListenerContext context, HttpListenerRequest request)
+        {
+
+            string url = request.Url.LocalPath.ToString();
+
+            MyMethods myMethods = new MyMethods();
+
+            HttpListenerResponse response = context.Response;
+
+            string responseString = "";
+
+                Type thisType = myMethods.GetType();
+                MethodInfo method = thisType.GetMethod("method2");
+                responseString = (string)method.Invoke(myMethods, null);
+            
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            output.Close();
+        }
+    }
+
+    public class HttpException : Exception
+    {
+        public HttpException(HttpStatusCode code, string message) : base(message)
+        {
+            Code = code;
+        }
+
+        public HttpStatusCode Code { get; }
     }
 }
